ccc79ba43784bb8dab85423b48581567cfb66083
Sharkty21 <tylermsharkey@gmail.com>
2023-08-21 08:37:27 -0500

    sync comunication between services

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,17 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+Console.WriteLine($"CommandService Endpoint {builder.Configuration.GetValue<string>("CommandService")}");
 
 var app = builder.Build();
 
